4e5d102bdba87f043be3f6263f790f473c6b6a76
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-04-05 09:42:38 -0700

    add buildArray() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
   $('button').click(function(){
-    var userInput = parseInt($('input').val());
-    $('output').append(userInput);
+    var userInput = $('input').val();
+    console.log(typeof userInput);
+    $('p').empty();
+    $('#output').append(buildArray(userInput));
   })
 })
 
+//buildArray() takes a userInput and builds an array of that length
+//It also substitues certain number strings for test strings
 function buildArray(number) {
   var originArray = [];
 
-  for (i = 0; i <= number; i++;)
+  for (i = 0; i <= number; i++){
+    if(i.toString().indexOf("3") != -1) {
+      originArray.push("I'm sorry, Dave. I can't do that.");
+    }
+    else if (i.toString().indexOf("2") != -1){
+      originArray.push(" boop! ");
+    }
+    else if (i.toString().indexOf("1") != -1) {
+      originArray.push(" beep! ");
+    }
+    else {
+      originArray.push(" " + i + " ");
+    }
+  }
+  return originArray;
 }

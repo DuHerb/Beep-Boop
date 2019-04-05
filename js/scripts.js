8f35b054ce91bf3bda01ef7ad30d9117cb3a2676
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-04-05 09:01:26 -0700

    add base styles, html, and scripts to project

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('button').click(function(){
+    var userInput = parseInt($('input').val());
+    $('output').append(userInput);
+  })
+})
+
+function buildArray(number) {
+  var originArray = [];
+
+  for (i = 0; i <= number; i++;)
+}

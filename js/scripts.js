77d23ffce0c723a04ed8148d438f3ab38185464d
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-04-05 17:22:06 -0700

    add major styling to html page, clean up scripts.js. some images resized

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,13 @@ $(document).ready(function(){
   $('button').click(function(){
     var userInput = $('input').val();
     $('h1').empty();
-    // console.log(buildArray(userInput));
-    // $('#output').append(buildArray(userInput));
-    var testArray = buildArray(userInput);
-    console.log(testArray.join(""));
-    console.log(typeof testArray.join(""));
-    var typeArray = [testArray.join("")];
+    var originArray = buildArray(userInput);
+    var outputArray = [originArray.join("")];
 
-    // typeArray.push(testArray.join(""));
-    // console.log(typeArray);
-    typeWriter("output", typeArray);
+    typeWriter("output", outputArray);
+    $('input').val('');
   })
-  $('#reset').click(function(){
-    window.location.reload();
-  })
-
+})
 
 //buildArray() takes a userInput and builds an array of that length
 //It also substitues certain number strings for test strings
@@ -28,10 +20,10 @@ function buildArray(number) {
       originArray.push("I'm sorry, Dave. I can't do that.");
     }
     else if (q.toString().indexOf("2") != -1){
-      originArray.push(" boop! ");
+      originArray.push(" boop ");
     }
     else if (q.toString().indexOf("1") != -1) {
-      originArray.push(" beep! ");
+      originArray.push(" beep ");
     }
     else {
       originArray.push(" " + q + " ");
@@ -43,11 +35,7 @@ function buildArray(number) {
 //typewriter effects
 // values to keep track of the number of letters typed, which quote to use. etc. Don't change these values.
 var i = 0,
-    a = 0,
-    isBackspacing = false,
-    isParagraph = false;
-
-// Typerwrite text content. Use a pipe to indicate the start of the second line "|".
+    a = 0;
 
 // Speed (in milliseconds) of typing.
 var speedForward = 50, //Typing Speed
@@ -56,75 +44,20 @@ var speedForward = 50, //Typing Speed
     speedBackspace = 0; //Backspace Speed
 
 //Run the loop
-
-
 function typeWriter(id, ar) {
   var element = $("#" + id),
       aString = ar[a],
       eHeader = element.children("h1"), //Header element
       eParagraph = element.children("p"); //Subheader element
 
-  // Determine if animation should be typing or backspacing
-  if (!isBackspacing) {
+  if (i < aString.length) {
+      eHeader.text(eHeader.text() + aString.charAt(i));
+      i++;
+      setTimeout(function(){ typeWriter(id, ar); }, speedForward);
 
-    // If full string hasn't yet been typed out, continue typing
-    if (i < aString.length) {
-
-      // If character about to be typed is a pipe, switch to second line and continue.
-      if (aString.charAt(i) == "|") {
-        isParagraph = true;
-        eHeader.removeClass("cursor");
-        eParagraph.addClass("cursor");
-        i++;
-        setTimeout(function(){ typeWriter(id, ar); }, speedBetweenLines);
-
-      // If character isn't a pipe, continue typing.
-      } else {
-        // Type header or subheader depending on whether pipe has been detected
-        if (!isParagraph) {
-          eHeader.text(eHeader.text() + aString.charAt(i));
-        } else {
-          eParagraph.text(eParagraph.text() + aString.charAt(i));
-        }
-        i++;
-        setTimeout(function(){ typeWriter(id, ar); }, speedForward);
-      }
-
-    // If full string has been typed, switch to backspace mode.
     } else if (i == aString.length) {
-
       i=0;
       a=0;
       return;
     }
-
-  // If backspacing is enabled
-  }
-  // else {
-
-    // If either the header or the paragraph still has text, continue backspacing
-    // if (eHeader.text().length > 0 || eParagraph.text().length > 0) {
-
-      // If paragraph still has text, continue erasing, otherwise switch to the header.
-      // if (eParagraph.text().length > 0) {
-      //   eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));
-      // } else if (eHeader.text().length > 0) {
-      //   eParagraph.removeClass("cursor");
-      //   eHeader.addClass("cursor");
-      //   eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
-      // }
-      // setTimeout(function(){ typeWriter(id, ar); }, speedBackspace);
-
-    // If neither head or paragraph still has text, switch to next quote in array and start typing.
-    // } else {
-
-      // isBackspacing = false;
-      // i = 0;
-      // isParagraph = false;
-      // a = (a + 1) % ar.length; //Moves to next position in array, always looping back to 0
-      // setTimeout(function(){ typeWriter(id, ar); }, 50);
-
-    }
-  // }
-// }
-})
+}
